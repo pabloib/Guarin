25e36ec0aacfe43419cfb38a203203970bb46303
Jonathan <jgg.9026@gmail.com>
2015-10-18 05:36:29 -0500

    se agrego el mapa

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -110,6 +110,42 @@ angular.module('starter.controllers', ['starter.services', 'firebase'])
 //   ];
 // })
 
+
+.controller('MarkerRemoveCtrl', function($scope, $ionicLoading) {
+
+  $scope.positions = [{
+    lat: 43.07493,
+    lng: -89.381388
+  }];
+
+  $scope.$on('mapInitialized', function(event, map) {
+    $scope.map = map;
+  });
+
+  $scope.centerOnMe= function(){
+  $scope.positions = [];
+    
+    
+    $ionicLoading.show({
+      template: 'Loading...'
+    });
+
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      $scope.positions.push({lat: pos.k,lng: pos.B});
+      console.log(pos);
+      $scope.map.setCenter(pos);
+      $ionicLoading.hide();
+    });
+
+  };
+
+})
+
+
+
+
 .controller('SessionsCtrl', function($scope, Session) {
     $scope.sessions = Session.query();
 })
@@ -183,5 +219,7 @@ angular.module('starter.controllers', ['starter.services', 'firebase'])
 //   }
 // });
   };
+
+
 
 });
